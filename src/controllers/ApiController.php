37021f2e0207b178a16fa3c3b3334ca72df5cc36
Pavel Angelov <psangelov@gmail.com>
2018-12-20 11:28:06 +0200

    Add files via upload

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -5,27 +5,121 @@ use Slim\Http\Response;
 
 class ApiController {
 
+    // mysql pdo
     private $db;
+    // array of allowed tables to query
+    private $allowedTables = ['nas', 'radacct', 'radgroupcheck'];
+    // default status code
+    private $statusCode = 200;
 
     public function __construct($db) {
         $this->db = $db;
     }
 
-    public function readAction(Request $request, Response $response, $args)
-    {
-        $selectStatement = $this->db->select()
-                               ->from($request->getAttribute('table')/*'radacct'*/);
+    // function to read records from database
+    public function readAction(Request $request, Response $response, $args) {
+        $table = $request->getAttribute('table');
+        $id = $request->getAttribute('id');
+        // check if table param value is allowed read
+        if (in_array($table, $this->allowedTables)) {
+            // build the query
+            $selectStatement = $this->db->select()->from($table);
+            // check if we have and id to the query
+            if ($id && is_numeric($id)) {
+                $selectStatement->where('id', '=', $id);
+            }
+            // execute query
+            $stmt = $selectStatement->execute();
+            // fetch rows (all)
+            $data = $stmt->fetchAll();   
+        } else {
+            // prepare error status code and message
+            $data = ['message' => 'Requried table not allowed'];
+            $this->statusCode = 401;
+        }
 
-        $stmt = $selectStatement->execute();
-        $data = $stmt->fetchAll();
-
-
-        /*$response->withStatus(200);
-        $response->withHeader('Content-Type', 'application/json');
-        $response->write(json_encode($data));*/
-        $response->withJson($data, 200);
-
-        return $response->withJson($data, 200);
+        return $response->withJson($data, $this->statusCode);
     }
 
+    // function to update records from database
+    public function updateAction(Request $request, Response $response, $args) {
+        $table = $request->getAttribute('table');
+        $id = $request->getAttribute('id');
+        $body = $request->getParsedBody();
+        // check if table param value is allowed read
+        if (in_array($table, $this->allowedTables) && is_numeric($id) && count($body)) {
+            // build the query
+            $updateStatement = $this->db->update($body)
+                                        ->table($table)
+                                        ->where('id', '=', $id);
+            // execute query
+            try {
+                $stmt = $updateStatement->execute();
+                $data = ['message' => 'Record updated'];
+            } catch (\PDOException $e) {
+                $data = ['message' => $e->getMessage()];
+                $this->statusCode = 400;
+            }
+        } else {
+            // prepare error status code and message
+            $data = ['message' => 'Requried table not allowed'];
+            $this->statusCode = 401;
+        }
+
+        return $response->withJson($data, $this->statusCode);
+    }
+
+    // function to insert new records to database
+    public function insertAction(Request $request, Response $response, $args) {
+        $table = $request->getAttribute('table');
+        $body = $request->getParsedBody();
+        // check if table param value is allowed read
+        if (in_array($table, $this->allowedTables) && is_numeric($id)) {
+            // build the query
+            $insertStatement = $this->db->insert(array_keys($body))
+                                        ->into($table)
+                                        ->values(array_values($body));
+            // execute query
+            try {
+                $insertStatement->execute();
+                $data = ['message' => 'Record inserted'];
+            } catch (\PDOException $e) {
+                $data = ['message' => $e->getMessage()];
+                $this->statusCode = 400;
+            }
+        } else {
+            // prepare error status code and message
+            $data = ['message' => 'Requried table not allowed'];
+            $this->statusCode = 401;
+        }
+
+        return $response->withJson($data, $this->statusCode);
+    }
+
+    // function to delete records from database
+    public function deleteAction(Request $request, Response $response, $args) {
+        $table = $request->getAttribute('table');
+        $id = $request->getAttribute('id');
+        // check if table param value is allowed read
+        if (in_array($table, $this->allowedTables)) {
+            // build the query
+            $deleteStatement = $this->db->delete()
+                                        ->from($table)
+                                        ->where('id', '=', $id);            
+            // execute query
+            try {
+                $stmt = $deleteStatement->execute();
+                $data = ['message' => 'Record deleted'];
+            } catch (\PDOException $e) {
+                $data = ['message' => $e->getMessage()];
+                $this->statusCode = 400;
+            }
+        } else {
+            // prepare error status code and message
+            $data = ['message' => 'Requried table not allowed'];
+            $this->statusCode = 401;
+        }
+
+        return $response->withJson($data, $this->statusCode);
+    }
 }
